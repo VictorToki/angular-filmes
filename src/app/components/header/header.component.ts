93a1cb711d643171f4858d7e3a467aa286d91b6e
Victor <victorkiam@msn.com>
2023-02-06 12:54:09 -0300

    Acessibilidade: fontsize

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
+  constructor(){
+  }
+
+  setFontSize(index){
+    const elm = document.querySelector<HTMLElement>('.pesquisar-filme-content');
+
+    switch(index){
+      case 0:
+        elm.style.fontSize = '1rem';
+        break;
+      case 1:
+        elm.style.fontSize = '1.35rem';
+        break;
+      case 2:
+        elm.style.fontSize = '1.85rem';
+        break;
+    }
+  }
+
+  onButtonGroupClick($event){
+    let clickedElement = $event.target || $event.srcElement;
+
+    if( clickedElement.nodeName === "BUTTON" ) {
+
+      let isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(".active");
+      if( isCertainButtonAlreadyActive ) {
+        isCertainButtonAlreadyActive.classList.remove("active");
+      }
+
+      clickedElement.className += " active";
+    }
+
+  }
 
 }
